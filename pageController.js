1cc48b445886c93651e749839c2035cef5acb4e9
s-ritwik <94957785+s-ritwik@users.noreply.github.com>
2023-07-27 15:48:23 +0530

    pre final

--- pageController.js
+++ pageController.js
@@ -1,10 +1,39 @@
 const pageScraper= require('./pageScraper');
+const fs=require('fs')//jsonn
+
 
 async function scrapeAll(browserInstance){
     let browser;
     try{
         browser=await browserInstance;
-        await pageScraper.scraper(browser);
+        //for scraping particular books uncommnet following code
+        let scrapedData = {};
+		// Call the scraper for different set of books to be scraped,for all scraping uncomment
+		scrapedData['Travel'] = await pageScraper.scraper(browser, 'Travel');
+        scrapedData['Mystery'] = await pageScraper.scraper(browser, 'Mystery');
+
+		await browser.close();
+       // const fs = require('fs');
+
+// Assuming scrapedData is an object with key-value pairs
+
+// Convert each property (which should be an object) to a JSON string and add a new line character after each one
+const jsonStringArray = [];
+for (const key in scrapedData) {
+  if (Object.hasOwnProperty.call(scrapedData, key)) {
+    const data = scrapedData[key];
+    jsonStringArray.push(JSON.stringify(data) + '\n');
+  }
+}
+
+fs.writeFile('data.json', jsonStringArray.join(''), 'utf8', function(err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("The data has been scraped and saved successfully! View it at './data.json'");
+});
+
+        //await pageScraper.scraper(browser);
     }
     catch(err)
     {
--- pageScraper.js
+++ pageScraper.js
@@ -1,13 +1,27 @@
 const scraperObject =
 {
     url: 'http://books.toscrape.com',
-    async scraper(browser)
+    async scraper(browser,category)
     {
         let page =await browser.newPage();
         console.log(`going to ${this.url}--`);
         await page.goto(this.url);
 
+        let selectedCategory = await page.$$eval('.side_categories > ul > li > ul > li > a', (links, _category) => {
+
+			// Search for the element that has the matching text
+			links = links.map(a => a.textContent.replace(/(\r\n\t|\n|\r|\t|^\s|\s$|\B\s|\s\B)/gm, "") === _category ? a : null);
+			let link = links.filter(tx => tx !== null)[0];
+			return link.href;
+		}, category);
+		// Navigate to the selected category
+		await page.goto(selectedCategory);
+
+
+        let scrapedData =[];//empty array for mutiple pages
         //wait for dom
+        async function scrapeCurrentPage()
+        {
         await page.waitForSelector('.page_inner');
         // get urls of all books
         let urls =await page.$$eval('section ol > li',links =>
@@ -15,7 +29,11 @@ const scraperObject =
             //book in stock?
             links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
             
-            links=links.map(el => el.querySelector('h3 > a').href)
+            try{links=links.map(el => el.querySelector('h3 > a').href)}
+            catch(err)
+            {
+                console.log("cant seperate files :",err);
+            }
             return links;
         });
         console.log(urls,"now we will scrap these sites");
@@ -46,9 +64,31 @@ const scraperObject =
         for(link in urls)
         {
             let currentPageData = await pagePromise(urls[link]);
-
-            console.log(currentPageData['bookTitle']);
+            scrapedData.push(currentPageData);
+           // console.log(currentPageData['bookTitle']);
         }
+        let nextButtonExist = false;
+        try{
+            const nextButton = await page.$eval('.next > a', a =>a.textContent);
+            nextButton= true;
+        }
+        catch(err)
+        {
+            nextButton=false;
+            console.log("END OF PAGES");
+        }
+        if(nextButtonExist)
+        {
+            await page.click('.next > a');
+            return scrapeCurrentPage();//rec call
+        }
+        await page.close();
+        return scrapedData;
+    }
+    let data = await scrapeCurrentPage();
+    console.log(data);
+    console.log("END OF PAGESjj");
+    return data;
     }   
 }
  module.exports= scraperObject;
